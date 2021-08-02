799c10302ea06903835b388410425dcab2b92ee3
theRealShantwon <theRealShantwon@gmail.com>
2021-08-02 13:25:21 -0400

    Initial Commit

---
+++ mobileTimeTest/mobileTimeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileTimeTestApp.swift
+//  mobileTimeTest
+//
+//  Created by Shannon Anthony on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct mobileTimeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
